735bc18c97eb7fc358916af1074ad5bc45111dbe
xxx <humengmingxx@gmail.com>
2022-04-24 23:03:31 +0800

    feat(todos)：add

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
-import { ExtensionContext } from 'vscode'
+import { ExtensionContext, workspace, window } from 'vscode'
 
 export function activate(context: ExtensionContext) {
-  // TODO
+  /**
+   * 实现思路：
+   *    1、获取当前编辑区域的所有文件的 path
+   *    2、提取符合条件的文件 path
+   *    3、获取符合条件的上级目录文件名
+   *    4、动态设置 tab label name
+   */
+
+  /**
+   * 调研的时候碰到了一个问题：没找到可以动态修改 label name 的方法
+   */
 }
 
 export function deactivate() { }
